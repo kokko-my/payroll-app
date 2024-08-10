4b8d678e3198cea6047a6ce52316c7a30bbee474
kokko-my <kota.mysa6505@gmail.com>
2024-08-10 15:58:06 +0900

    勤務先情報及び勤務時間情報の入力欄を作成

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 # app.py
 
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms.fields import (
+    SubmitField, StringField, IntegerField, TimeField
+)
+
+basedir = os.path.abspath(os.path.dirname(__name__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'myapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
+
+# 勤務先情報登録フォーム
+class WorkplaceForm(FlaskForm):
+    name = StringField('勤務先の名前: ')
+    hourly = IntegerField('時給: ')
+    submit = SubmitField('登録')
+
+# 勤務時間登録フォーム
+class WorktimeForm(FlaskForm):
+    start_time = TimeField('出勤時刻: ')
+    end_time = TimeField('退勤時刻: ')
+    submit = SubmitField('登録')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    workplace_form = WorkplaceForm(request.form)
+    worktime_form  = WorktimeForm(request.form)
+    return render_template(
+        'index.html',
+        workplace_form=workplace_form,
+        worktime_form=worktime_form
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
