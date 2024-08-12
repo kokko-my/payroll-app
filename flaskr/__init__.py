08fc69c178af7d9e609c523bcb0b23f215c27ec1
kokko-my <kota.mysa6505@gmail.com>
2024-08-12 16:01:25 +0900

    DBの設定を追加

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,12 @@
 # flaskr/__init__.py
 import os
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__name__))
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +15,6 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from flaskr.views import bp
     app.register_blueprint(bp)
+    db.init_app(app)
+    migrate.init_app(app, db)
     return app
